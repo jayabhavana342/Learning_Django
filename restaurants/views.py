a02b7e9d61d39622e9d628be0cc802978f509351
jayabhavana342 <j_k201@txstate.edu>
2018-01-09 10:14:38 -0600

    Finished 7-Render a Django Template Start of 8.-Context in Django Templates

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,27 +7,30 @@ from django.shortcuts import render
 # Create your views here.
 
 # function based view
+# def home(request):
+#     html_var = 'f strings'
+#     # should have f before """
+#
+#     html_ = """
+#
+#     <!DOCTYPE html>
+#     <html lang=en>
+#
+#     <head></head>
+#
+#     <body>
+#         <h1>Hello World!</h1>
+#         <p>This is {html_var} coming through</p>
+#     </body>
+#
+#     </html>
+#
+#     """
+#
+#     # f strings
+#
+#     return HttpResponse(html_)
+
 def home(request):
-    html_var = 'f strings'
-    # should have f before """
-
-    html_ = """
-    
-    <!DOCTYPE html>
-    <html lang=en>
-    
-    <head></head>
-    
-    <body>
-        <h1>Hello World!</h1>
-        <p>This is {html_var} coming through</p>
-    </body>
-    
-    </html>
-    
-    """
-
-    # f strings
-
-    return HttpResponse(html_)
-    # return render(request, "home.html", {})#response
+    num = random.randint(0,100)
+    return render(request, "base.html", {"html_var": num})#response
