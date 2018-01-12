ccdefd5d6c807365af326fcb071b0a04dd0fe247
jayabhavana342 <j_k201@txstate.edu>
2018-01-12 11:01:06 -0600

    Finished 19-Restaurant Profile Detail

--- Learning_Django/urls.py
+++ Learning_Django/urls.py
@@ -21,13 +21,15 @@ from django.views.generic import TemplateView
 from restaurants.views import (
     restaurant_listview,
     RestaurantListView,
+    RestaurantDetailView
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html')),
     path('restaurants/', RestaurantListView.as_view()),
-    path('restaurants/<slug:slug>/', RestaurantListView.as_view()),
+    # path('restaurants/<slug:slug>/', RestaurantListView.as_view()),
+    path('restaurants/<int:rest_id>/', RestaurantDetailView.as_view()),
     # path('restaurants/asian', AsianFusionRestaurantListView.as_view()),
     path('about/', TemplateView.as_view(template_name='about.html')),
     path('contact/', TemplateView.as_view(template_name='contact.html')),
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
@@ -19,6 +19,7 @@ def restaurant_listview(request):
 class RestaurantListView(ListView):
 
     def get_queryset(self):
+        print(self.kwargs)
         slug = self.kwargs.get("slug")
         if slug:
             queryset = RestaurantLocation.objects.filter(
@@ -28,3 +29,12 @@ class RestaurantListView(ListView):
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id) #pk = rest_id
+        return obj
+
