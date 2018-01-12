304a699fbd4a4c9a2dd83132d56753454912ad94
jayabhavana342 <j_k201@txstate.edu>
2018-01-12 14:35:42 -0600

    Finished 25 - The Power of Django Forms

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,36 +7,29 @@ from django.views.generic import TemplateView, ListView, DetailView
 from .models import RestaurantLocation
 from .forms import RestaurantCreateForm
 
+
 # Create your views here.
 
 def restaurant_createview(request):
-    print(request.GET)
-    print(request.POST)
+    form = RestaurantCreateForm(request.POST or None)
+    errors = None
 
-    if request.method == "POST":
-        print("post data")
-        print(request.POST)
-        title = request.POST.get("title")
-        location = request.POST.get("location")
-        category = request.POST.get("category")
-
+    if form.is_valid():
         obj = RestaurantLocation.objects.create(
-            name=title,
-            location=location,
-            category=category
-
+            name=form.cleaned_data.get('name'),
+            location=form.cleaned_data.get('location'),
+            category=form.cleaned_data.get('category')
         )
         return HttpResponseRedirect("/restaurants/")
 
-    if request.method == "GET":
-        print("get data")
-
-
+    if form.errors:
+        errors = form.errors
 
     template_name = 'restaurants/form.html'
     queryset = RestaurantLocation.objects.all()
-    context = {}
+    context = {"form": form, "errors": errors}
     return render(request, template_name, context)
+
 
 def restaurant_listview(request):
     template_name = 'restaurants/restaurants_list.html'
@@ -45,6 +38,7 @@ def restaurant_listview(request):
         "object_list": queryset
     }
     return render(request, template_name, context)
+
 
 class RestaurantListView(ListView):
 
@@ -60,6 +54,7 @@ class RestaurantListView(ListView):
             queryset = RestaurantLocation.objects.all()
         return queryset
 
+
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
     print(queryset)
@@ -68,4 +63,3 @@ class RestaurantDetailView(DetailView):
     #     rest_id = self.kwargs.get('rest_id')
     #     obj = get_object_or_404(RestaurantLocation, id=rest_id) #pk = rest_id
     #     return obj
-
